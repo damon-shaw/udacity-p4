9c50d5dc5f9e93be0598a92cc5f34f27e51c2ab7
EC2 Default User <ec2-user@ip-172-31-19-193.ec2.internal>
2021-09-01 02:28:58 +0000

    complete run_kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -2,17 +2,23 @@
 
 # This tags and uploads an image to Docker Hub
 
+USER=vanthios
+
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=$USER/udacity-p4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run boston-predict-api\
+	--image=$dockerpath\
+	--port=80\
+	--labels app=boston-predict-api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward boston-predict-api 8000:80
